92f0d77caf33c7b8c6a1bcbccef99e172cac6b6e
tedstar25 <you@example.com>
2024-01-31 14:53:22 +0100

    added subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 
 
-export const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
+export const Button = ({label, iconURL, backgroundColor, textColor, borderColor, fullWidth}) => {
 
 
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor} ` : 'bg-coral-red  text-white '} rounded-full `}>
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor} ` : 'bg-coral-red  text-white '} rounded-full ${fullWidth && "w-full"}`}>
         {label}
          {iconURL && <img
          src={iconURL}
