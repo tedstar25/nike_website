6a6c91d01f2e3e97372ca87ff25c01c81eba3bef
tedstar25 <you@example.com>
2024-01-31 12:24:46 +0100

    added special offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 
 
-export const Button = ({label, iconURL}) => {
+export const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
+
+
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor} ` : 'bg-coral-red  text-white '} rounded-full `}>
         {label}
          {iconURL && <img
          src={iconURL}
